48e938d2d0be7b0ec2184b39b3ef8994b0314c5f
waferris <waferris2@gmail.com>
2021-12-15 22:46:34 -0500

    add tailwind and style starter components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 
 const IndexPage: NextPage = () => {
   return (
-    <Layout useBackdrop={false}>
+    <Layout usePadding={false}>
       <Auth />
     </Layout>
   );
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps, NextPage } from "next";
+import Link from "next/link";
 import Layout from "../components/Layout";
 import { useAuthWithRedir, ProtectedRoute } from "../lib/auth";
 
@@ -11,21 +12,33 @@ const ProfilePage: NextPage = (/* props from SSR */) => {
 
   return (
     <Layout useBackdrop={false}>
-      <div className="h-screen flex flex-col justify-center items-center relative">
-        <h2 className="text-3xl my-4">
-          Howdie, {user && user.email ? user.email : "Explorer"}!
-        </h2>
-        {!user && <small>You've landed on a protected page. Please</small>}
-        {user && (
-          <div>
-            <button
-              onClick={signOut}
-              className="border bg-gray-500 border-gray-600 text-white px-3 py-2 rounded w-full text-center transition duration-150 shadow-lg"
-            >
-              Sign Out
-            </button>
+      <div className="bg-white py-16 sm:py-24">
+        <div className="relative sm:py-16">
+          <div className="text-center justify-center items-center">
+            <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">
+              Profile (SSR)
+            </h2>
+            <p className="mt-2 text-3xl font-bold text-gray-900 sm:text-4xl sm:tracking-tight lg:text-4xl">
+              Howdie, {user && user.email ? user.email : "Explorer"}!
+            </p>
+            {user ? (
+              <button
+                onClick={signOut}
+                className="mt-8 block m-auto rounded-md border border-transparent px-5 py-3 bg-indigo-500 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10"
+              >
+                Sign Out
+              </button>
+            ) : (
+              <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+                You've landed on a protected page. Please{" "}
+                <span className="underline">
+                  <Link href="/">log in</Link>
+                </span>{" "}
+                to view the page's full content
+              </p>
+            )}
           </div>
-        )}
+        </div>
       </div>
     </Layout>
   );
